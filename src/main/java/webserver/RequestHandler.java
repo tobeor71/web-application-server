56a7948f4004ca0e3b5b4f12d420afd533df25cc
Ham Seung Woo <tobeor71@gmail.com>
2026-09-17 09:54:04 +0900

    요구사항 4번 구현(BufferReader)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -35,13 +36,26 @@ public class RequestHandler extends Thread {
             }
 
             String[] tokens = line.split(" ");
+            int contentLength = 0;
 
             while(!line.equals("")) {
-                line = br.readLine();
                 log.debug("header : {}", line);
+                line = br.readLine();
+                if(line.contains("Content-Length")) {
+                    contentLength = getContentLength(line);
+                }
             }
 
             String url = tokens[1];
+            if("user/create".equals(url)) {
+                String body = IOUtils.readData(br, contentLength);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("user : {}", user);
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos, "/index.html");
+            }
+
             if("/user/create".startsWith(url)) {
                 int indoex = url.indexOf("?");
                 String queryString = url.substring(indoex + 1);
@@ -58,6 +72,21 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void response302Header(DataOutputStream dos, String url){
+        try{
+            dos.writeBytes("HTTP/1.1 02 Redirect \r\n");
+            dos.writeBytes("Location: "  + url + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private int getContentLength(String line){
+        String[] headerTokens = line.split(":");
+        return Integer.parseInt(headerTokens[1].trim());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
